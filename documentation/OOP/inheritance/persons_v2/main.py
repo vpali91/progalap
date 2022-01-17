d3152fcdd8f66bf0667b3edc655a6d15517c9d8e
vpali91 <44944542+vpali91@users.noreply.github.com>
2022-01-17 05:49:07 +0100

    Create main.py
    Futtatható fájl

---
+++ documentation/OOP/inheritance/persons_v2/main.py
@@ -0,0 +1,6 @@
+from employee import Employee                  
+# objektum példányosítása
+a = Employee('Sanyi', 886012, 200000, "Gyakornok")    
+  
+# az objektumhoz tartozó funkció meghívása
+a.display() 
