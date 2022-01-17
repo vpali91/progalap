9f29bfbee8be28185d806fca7f7f11a870feea57
vpali91 <44944542+vpali91@users.noreply.github.com>
2022-01-17 06:36:33 +0100

    Create madar.py
    Származtatott, de egyben szülő osztály is

---
+++ documentation/OOP/inheritance/allatok/madar.py
@@ -0,0 +1,19 @@
+from allat import Allat
+
+# Származtatott osztály: Inherited or Sub class
+# Példánkban ez az osztály közvetlenül az ősosztálytól származik és az ebből leszármazó osztálynak a szülő/parent osztálya
+class Madar(Allat):
+      
+    # Constructor
+    def __init__(self, nev, suly, kor, tollassag):
+        Allat.__init__(self,nev, suly, kor)
+        self.tollassag = tollassag
+  
+    def toll_ritkul(self):
+        self.tollassag += 1
+
+    def tojik(self):
+        self.suly -= 1
+
+    def csip(self):
+        print("Megcsiplek")
