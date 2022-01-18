a06713540420b67305d9e4643b89b981f0d4297b
vpali91 <44944542+vpali91@users.noreply.github.com>
2022-01-18 12:30:54 +0100

    Create macska.py

---
+++ documentation/OOP/inheritance/multiple_inheritance/macska.py
@@ -0,0 +1,16 @@
+from emlos import Emlos
+
+# Származtatott osztály: Inherited or Sub class
+# Példánkban ez az osztály közvetetten az Allat osztályból származik az Emlős osztályon keresztül
+class Macska(Emlos):
+      
+    # Constructor
+    def __init__(self, nev, suly, kor, szorosseg, fajta):
+        Emlos.__init__(self,nev, suly, kor, szorosseg)
+        self.fajta = fajta
+
+    def nyavog(self):
+        print("Meow")
+
+    def karmol(self):
+        print("Megkarmoltalak")
