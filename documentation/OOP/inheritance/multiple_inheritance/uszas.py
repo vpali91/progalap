00f0e679c231319b9b8ebfbd33af02ec598906fd
vpali91 <44944542+vpali91@users.noreply.github.com>
2022-01-18 12:31:34 +0100

    Create uszas.py

---
+++ documentation/OOP/inheritance/multiple_inheritance/uszas.py
@@ -0,0 +1,7 @@
+class Uszas(object):
+    def __init__(self,sebesseg,idotartam):
+        self.uszas_sebesseg = sebesseg
+        self.uszas_idotartam = idotartam
+
+    def uszik(self):
+        print("Úszok!")
