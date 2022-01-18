2f2da9d799de477c338605dfc02fb9d79192d32e
vpali91 <44944542+vpali91@users.noreply.github.com>
2022-01-18 12:31:13 +0100

    Create repul.py

---
+++ documentation/OOP/inheritance/multiple_inheritance/repul.py
@@ -0,0 +1,7 @@
+class Repul(object):
+    def __init__(self, sebesseg, idotartam):
+        self.sebesseg = sebesseg
+        self.idotartam = idotartam
+
+    def repules(self):
+        print("Repülök!")
