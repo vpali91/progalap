6d0856c0453bcc8fc139ec649e53ab9c702986e5
vpali91 <44944542+vpali91@users.noreply.github.com>
2022-01-19 12:50:08 +0100

    Delete segedletek/feladat2 directory

--- segedletek/feladat2/calculation.py
+++
@@ -1,9 +0,0 @@
-class Calculation:
-    def __init__(self, ber):
-        self.netto = ber
-        self.diak = 0
-        self.rendes_mv = 0
-
-    def szamolas(self):
-        self.diak = self.netto*1.15
-        self.rendes_mv = self.netto*1.83
