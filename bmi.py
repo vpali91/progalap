b5d79572ac3a6d8016db86bae94f701dc3208081
vpali91 <44944542+vpali91@users.noreply.github.com>
2022-06-10 17:11:39 +0200

    Delete bmi.py

--- bmi.py
+++
@@ -1,6 +0,0 @@
-#BMI index, meg kell adni a magasságod méterben(float) és a súlyod egész szám kg-ban(int), ez alapján a testsúlyt elosztod a magassággal és az így kapott eredményt négyzetre emeled(**).
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-
-bmi = int(weight)/float(height)**2
-print(bmi)
