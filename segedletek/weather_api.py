bd5fe0768c681d626b4893c8f9caeda60208b3f4
vpali91 <44944542+vpali91@users.noreply.github.com>
2022-10-05 12:20:07 +0200

    Create weather_api.py

---
+++ segedletek/weather_api.py
@@ -0,0 +1,44 @@
+{
+"coord": {
+"lon": 20.1482,
+"lat": 46.253
+},
+"weather": [
+{
+"id": 804,
+"main": "Clouds",
+"description": "overcast clouds",
+"icon": "04d"
+}
+],
+"base": "stations",
+"main": {
+"temp": 289.99,
+"feels_like": 289.01,
+"temp_min": 289.99,
+"temp_max": 289.99,
+"pressure": 1029,
+"humidity": 49,
+"sea_level": 1029,
+"grnd_level": 1019
+},
+"visibility": 10000,
+"wind": {
+"speed": 2.57,
+"deg": 195,
+"gust": 2.93
+},
+"clouds": {
+"all": 92
+},
+"dt": 1664963050,
+"sys": {
+"country": "HU",
+"sunrise": 1664944956,
+"sunset": 1664986377
+},
+"timezone": 7200,
+"id": 715429,
+"name": "Szeged",
+"cod": 200
+}
