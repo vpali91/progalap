f673e401f58be762ef4b3fb5eaa44186b2933652
vpali91 <44944542+vpali91@users.noreply.github.com>
2022-10-12 09:19:10 +0200

    Create dictionary_list.py

---
+++ segedletek/dictionary_list.py
@@ -0,0 +1,81 @@
+test_data=[
+{
+    "n": "1",
+    "id": "5a8cd65c-ed68-45e7-8ff1-d13f61cd16c7",
+    "name": "Lindamarie Drees",
+    "address": "Letting Street 5247, Husser, Italy, 844652",
+    "birthDate": "03.09.1973",
+    "email": "herbert_needhamww1q@antenna.tlu"
+},
+{
+    "n": "2",
+    "id": "731c8a92-ff49-4744-82dd-c0c77d4473c2",
+    "name": "Luvia Cork",
+    "address": "Naval Street 5214, Opp, Trinidad And Tobago, 330034",
+    "birthDate": "25.07.1993",
+    "email": "abram_schwanket@tells.is"
+},
+{
+    "n": "3",
+    "id": "9ce8a143-3989-4ae2-ad47-c9fe74e5bfe0",
+    "name": "Aundria Keegan",
+    "address": "Travelers Road 1272, Kusatsu, France, 039311",
+    "birthDate": "31.05.1991",
+    "email": "kendricks_grandeltyg@baptist.dti"
+},
+{
+    "n": "4",
+    "id": "fe28c156-58fd-4d4b-846a-5f5293221da5",
+    "name": "Nathaiel Grasso",
+    "address": "Total Road 1, Malott, Turks And Caicos Islands, 461085",
+    "birthDate": "09.12.2003",
+    "email": "princeston_kielyrsst@pie.oj"
+},
+{
+    "n": "5",
+    "id": "efdd7abb-8f46-446a-a369-4d29470965ff",
+    "name": "Marsell Delia",
+    "address": "Addresses Road 7448, Adelphi, Bosnia And Herzegovina, 047861",
+    "birthDate": "05.05.1970",
+    "email": "tenna_landerkins@poet.ue"
+},
+{
+    "n": "6",
+    "id": "2aa3bc0a-f477-447e-8720-7935769a8030",
+    "name": "Jejuan Crunk",
+    "address": "Makers Road 931, Tariba, Somalia, 061293",
+    "birthDate": "12.03.2020",
+    "email": "huriel_poppd@org.wk"
+},
+{
+    "n": "7",
+    "id": "54ef2e36-97cb-4d89-9501-62e2d9071cfa",
+    "name": "Katima Vaughan",
+    "address": "Good Street 6598, Little Suamico, Latvia, 968213",
+    "birthDate": "09.01.2002",
+    "email": "lakoya_draytons@believe.at"
+},
+{
+    "n": "8",
+    "id": "d7ac29e6-24ec-48a9-b747-699866591c56",
+    "name": "Janey Gallop",
+    "address": "Anaheim St 3135, Skellytown, Jordan, 122269",
+    "birthDate": "24.02.1999",
+    "email": "jorell_cyphers2u@external.kf"
+},
+{
+    "n": "9",
+    "id": "df7aeb14-11a4-459c-858f-200629c5cf3b",
+    "name": "Mauria Monday",
+    "address": "Circle St 8031, Limestone Ridges, Philippines, 091751",
+    "birthDate": "05.10.2003",
+    "email": "porshea_orenjr@chemicals.le"
+},
+{
+    "n": "10",
+    "id": "6e76ce44-0454-46e9-92fb-7b0e22234d28",
+    "name": "Toria Fugate",
+    "address": "Strategy Road 2100, Sherburne, Netherlands Antilles, 894804",
+    "birthDate": "23.03.1998",
+    "email": "tashea_kephartmcrk@change.zot"
+}]
